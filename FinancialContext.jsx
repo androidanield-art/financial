d1ecd9cb539e7ec6c525eaf2fdb776d1061289dd
androidanield-art <androidanield@gmail.com>
2026-06-08 15:16:46 -0300

    as

--- FinancialContext.jsx
+++ FinancialContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useMemo, useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from './supabase';
 import { calculateFinances } from '../utils/financialCalculations';
 
 const FinancialContext = createContext();
